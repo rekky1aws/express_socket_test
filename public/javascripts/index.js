406fd3ede827f437c59425403ac0909e26fede27
rekky1aws <killikomfr@gmail.com>
2025-08-27 04:58:02 +0200

    Frontend name logging clean
    Removing list before adding new names to avoid doubles

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,6 +5,8 @@ console.log(socket);
 const peopleList = document.querySelector('#people-list');
 
 socket.on('updatePeople', (people) => {
+  peopleList.innerHTML = "";
+
   for (const id in people) {
     const personElt = document.createElement('div');
     personElt.classList.add('person');
