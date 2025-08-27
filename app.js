9deeec19b555faf62ff4e167d5f5269775225744
rekky1aws <killikomfr@gmail.com>
2025-08-27 04:35:45 +0200

    Fancier Hello World
    Sending /public/index.html when users access to app via route /

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
+
+// Socket.io setup
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
 const port = 3000;
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('a user has connected');
+});
+
+server.listen(port, () => {
     console.log(`Example app running on port : ${port}`);
 });
